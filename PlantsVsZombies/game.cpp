89f47c609a77f54813b07d4d7035ec2a0fff674a
耘硕 张 <SJMbaiyang@163.com>
2023-07-05 10:02:06 +0800

    update

--- PlantsVsZombies/game.cpp
+++ PlantsVsZombies/game.cpp
@@ -1,17 +1,28 @@
 #include "game.h"
 
-#define WIN_WID 900
+#define WIN_WID 1000
 #define WIN_HIG 600
+#define curX00 180							//第一行第一列草坪块的左上角位置的x值（减去120）（原256）
+#define curY00 95							//第一行第一列草坪块的左上角位置的y值
+#define cur_Height 100						//每一个草坪块的x的长度
+#define cur_Width 81					    //每一个草坪块的y的长度
 
 enum { PeaShooter, SunFlower, WallNut, PotatoMine, CherryBomb, CardCount };
-IMAGE* plant[CardCount][20];
+IMAGE* Plants[CardCount][20];
 bool judgePlant = false;		//判断是否捡起植物
 int curX, curY;					//当前植物移动过程中的位置
 bool fileExist(char* name);		//判断文件是否存在
 int index = -1;					//当前植物索引值
-IMAGE back_ground_img;
-IMAGE bar_img;
-IMAGE card_img[CardCount];
+IMAGE back_ground_img;			//游戏背景图片
+IMAGE bar_img;					//植物卡槽图片
+IMAGE card_img[CardCount];		//植物卡片图片
+//植物
+struct plant {
+	int type;					//植物种类，0：无
+	int frame;					//当前是第几帧
+};
+//全地图植物数组
+struct plant AllMap[5][9] = {0};
 
 bool fileExist(char* name) {
 	FILE* fp = fopen(name, "r");
@@ -30,7 +41,8 @@ void InitGame() {
 	//加载植物卡槽
 	loadimage(&bar_img, "res/bar5.png");
 
-	memset(plant, 0, sizeof(plant));
+	memset(Plants, 0, sizeof(Plants));
+	memset(AllMap, 0, sizeof(AllMap));
 	//加载植物卡片
 	for (int i = 0; i < CardCount; i++) {
 		char name[64];
@@ -40,8 +52,8 @@ void InitGame() {
 		for (int j = 0; j < 20; j++) {
 			sprintf_s(name,sizeof(name),"res/Plants/%d/%d.png",i,j+1);
 			if (fileExist(name)) {
-				plant[i][j] = new IMAGE;
-				loadimage(plant[i][j], name);
+				Plants[i][j] = new IMAGE;
+				loadimage(Plants[i][j], name);
 			}
 			else {
 				break;
@@ -49,8 +61,10 @@ void InitGame() {
 		}
 	}
 
+
+
 	//创建游戏窗口
-	initgraph(WIN_WID, WIN_HIG);
+	initgraph(WIN_WID, WIN_HIG, EX_SHOWCONSOLE);
 }
 
 void UpdateWindow() {
@@ -67,22 +81,38 @@ void UpdateWindow() {
 	}
 
 	//拖动过程中的植物
-	if (index >= 0) {
-		putimagePNG(curX,curY,plant[index][0]);
+	if (index >= 1) {
+		IMAGE* zhiwu = Plants[index-1][0];
+		putimagePNG(curX - zhiwu->getwidth()/2, curY - zhiwu->getheight()/2, zhiwu);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (AllMap[i][j].type > 0) {
+				int x = curX00 + j * cur_Width;
+				int y = curY00 + i * cur_Height;
+				int plant_type = AllMap[i][j].type - 1;
+				int page = AllMap[i][j].frame;
+				putimagePNG(x, y, Plants[plant_type][page]);
+			}
+		}
 	}
 
 	//结束缓冲
 	EndBatchDraw();
 }
 
+//点击判断
 void Click() {
 	ExMessage msg;
 	if (peekmessage(&msg)) {
+		//首次点击植物卡片
 		if (msg.message == WM_LBUTTONDOWN && judgePlant == false) {
 			if (msg.x > 338 && msg.x < 338 + 64 * CardCount && msg.y>6 && msg.y < 96) {
-				index = (msg.x - 338) / 64;
+				index = (msg.x - 338) / 64 + 1;
 				judgePlant = true;
-				printf("%d\n", index);
+				curX = msg.x;
+				curY = msg.y;
 			}
 		}
 		else if (msg.message == WM_MOUSEMOVE && judgePlant == true) {
@@ -90,7 +120,19 @@ void Click() {
 			curY = msg.y;
 		}
 		else if (msg.message == WM_LBUTTONDOWN && judgePlant == true) {
-			index = -1;
+			if (msg.x >= curX00 && msg.x <= curX00 + cur_Width * 9 && msg.y >= curY00 && msg.y <= curY00 + cur_Height * 5) {
+				int row = (msg.y - curY00) / cur_Height;
+				int col = (msg.x - curX00) / cur_Width;
+				
+				//种植
+				if (AllMap[row][col].type == 0) {
+					AllMap[row][col] = {
+						index,
+						0
+					};
+				}
+			}
+			index = 0;
 			judgePlant = false;
 		}
 	}
